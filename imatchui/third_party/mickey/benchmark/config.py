f73df056475e1f45057964715e7b779ed32f4eb7
Realcat <alpharealcat@gmail.com>
2024-10-13 11:39:03 +0000

    update: submodule

--- imatchui/third_party/mickey/benchmark/config.py
+++
@@ -1,8 +0,0 @@
-# translation and rotation thresholds [meters, degrees]
-# used to compute Precision and AUC considering Pose Error
-t_threshold = 0.25
-R_threshold = 5
-
-# reprojection (VCRE) threshold [pixels]
-# used to compute Precision and AUC considering VCRE
-vcre_threshold = 90
